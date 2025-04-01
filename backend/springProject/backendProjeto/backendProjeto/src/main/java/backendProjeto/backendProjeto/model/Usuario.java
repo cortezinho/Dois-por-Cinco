a7a20be15015e291141cd0407b14d85dc27bdeff
jvitor <jvitor.pinheiro.garcia@gmail.com>
2025-04-01 15:40:26 -0300

    novas implementações no spring

--- backend/springProject/backendProjeto/backendProjeto/src/main/java/backendProjeto/backendProjeto/model/Usuario.java
+++ backend/springProject/backendProjeto/backendProjeto/src/main/java/backendProjeto/backendProjeto/model/Usuario.java
@@ -1,4 +1,30 @@
 package backendProjeto.backendProjeto.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String usuario;
+
+    @Column(nullable = false)
+    private String senha;
+
+    public Usuario(String nome, String cpf, String usuario, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
 }
